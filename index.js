7d2dfff89481f409c35c90eed5b48a5c56e77bfa
oCodding <86965917+oCodding@users.noreply.github.com>
2021-08-01 19:56:50 -0300

    Add files via upload

---
+++ index.js
@@ -0,0 +1,41 @@
+var express = require("express");
+var application = express();
+var handlebars = require("express-handlebars");
+var bodyParser = require("body-parser");
+
+// Conexão com o banco de dados.
+var knex = require("knex")({
+    client: "mysql2",
+    connection: {
+        host: "localhost",
+        user: "root",
+        password: "",
+        database: "nome_database"
+    }
+});
+module.exports = knex;
+
+// Configuração [handlebars, bodyParser].
+application.engine("handlebars", handlebars({defaultLayout: "main"}));
+application.set("view engine", "handlebars");
+application.use(bodyParser.urlencoded({extended: false}));
+application.use(bodyParser.json());
+application.get("/", function(req, res){
+    res.render("forming");
+});
+
+// Alteração no banco de dados, de 0 para 1 na steamhex escolhida.
+application.post("/aprovar", function(req, res){
+    if(req.body.steamhex != ""){
+        knex("users").where({nome_coluna_steamhex: steamhex}).update({nome_coluna_valor_whitelisted: 1}).then(data => {
+            console.log(data);
+        }).catch(err => {
+            console.log(err);
+        });
+    }else{
+        console.log("Não foi preenchido o campo da SteamHex.");
+    };
+});
+
+// Conexão aplicação
+application.listen(8081);
